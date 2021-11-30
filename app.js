451edae73836a068facb7c1ae7136d12dbc7126c
Philippe Ngom <philngom@gmail.com>
2021-11-29 20:55:35 -0800

    functionality complete, need to work more on CSS

--- app.js
+++ app.js
@@ -2,7 +2,64 @@
 
 // let state
 
-// set event listeners 
+// set event listeners
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
+
+
+  //get DOM elements
+const cups = document.querySelectorAll('.cup');
+const buttons = document.querySelectorAll('button');
+const correct = document.querySelector('#correct');
+const incorrect = document.querySelector('#incorrect');
+const total = document.querySelector('#total');
+
+  //declare state variable counters
+const hidingPlaces = [
+  'left',
+  'middle',
+  'right'
+];
+
+let correctCount = 0;
+let totalCount = 0;
+
+  //click events
+  for (const button of buttons) {
+    button.addEventListener('click', () => {
+      let correctSpot = randomCup(hidingPlaces);
+      handleGuess(button.textContent, correctSpot);
+    });
+  }
+
+  //functions
+  function randomCup(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+  }
+
+  function handleGuess(guess, correctSpot) {
+    resetCups();
+    totalCount++;
+    const correctCup = document.querySelector('.' + correctSpot);
+
+    correctCup.classList.add('show');
+    console.log(guess, correctSpot)
+    if (guess === correctSpot) {
+      correctCount++;
+    }
+    correct.textContent = correctCount;
+    incorrect.textContent = totalCount - correctCount;
+    total.textContent = totalCount;
+  }
+
+  function resetCups() {
+    for (const cup of cups) {
+      cup.classList.remove('show');
+    }
+  }
+
+
+
+
